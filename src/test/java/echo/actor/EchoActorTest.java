3e07777886622b22485483eadac50b73fdbe07bf
Uwe Krohn <krohn@fh-luebeck.de>
2021-03-08 15:41:15 +0100

    EchoActorTest

---
+++ src/test/java/echo/actor/EchoActorTest.java
@@ -0,0 +1,19 @@
+package echo.actor;
+
+import inout.ScriptWriter;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static actor.AskWithOutput.ask;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static list.List.list;
+public  class EchoActorTest {
+
+	@ParameterizedTest
+	@CsvSource({"'Hallo'",
+							"'HalloHallo'"})
+	public void test(String s) throws InterruptedException {
+
+		Thread.sleep(500);
+    assertEquals("bla","bla");
+	}
+}
