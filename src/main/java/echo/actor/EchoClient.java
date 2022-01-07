472a45af45ee22e78edba6e20612c00671012335
333900 <julian.hardekopf@stud-th.luebeck.de>
2022-01-07 15:25:38 +0100

    KW02

--- src/main/java/echo/actor/EchoClient.java
+++ src/main/java/echo/actor/EchoClient.java
@@ -12,7 +12,7 @@ public class EchoClient {
     public static void main(String[] args) throws Exception {
 
         Writer writer = ActorSystem.actorSelection(args[0], Integer.parseInt(args[1])).call();
-        Stream<String> stream = AskStream.ask(writer, "ECHO", 1000);
+        Stream<String> stream = AskStream.ask(writer, args[2], 1000);
         ConsoleWriter consoleWriter = ConsoleWriter.stdout();
         consoleWriter.printLine(stream.head());
 
