b4e5f291f4d245ebc1d86225f95baad071d68cd4
333900 <julian.hardekopf@stud-th.luebeck.de>
2022-01-11 15:56:42 +0100

    KW02

--- src/test/java/echo/actor/DistEchoActorTest.java
+++ src/test/java/echo/actor/DistEchoActorTest.java
@@ -3,10 +3,8 @@ package echo.actor;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import java.io.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.*;
 
 public class DistEchoActorTest {
 
